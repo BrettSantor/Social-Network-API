819575246495aeb7885f26a1cc4c376b67e9f3dc
BrettSantor <b.santor27@gmail.com>
2023-02-22 16:41:05 -0800

    start mongoose models

--- models/Thought.js
+++ models/Thought.js
@@ -1,16 +1,53 @@
-//todo thoughtText
+const mongoose = require('mongoose');
+const { Thought } = require('.');
+
+//! REACTION
+//todo reactionId
+    //? use mongooses objectId data type
+    //? default value is set to a new objectId
+//todo reactionBody
     //? string
     //? required
-    //? must be between 1-280 characters
-//todo createdAt
-    //? date
-    //? set default value to the current time stamp
-    //? use a getter method to format the timestamp on query
+    //? 280 character max
 //todo Username
     //? string
     //? required
-//todo reactions
+//todo createdAt
+    //? date
+    //? set default to the current timestamp 
+    //? use a getter method to format the time stamp on query
+    //! schema settings
+        // not a model but will be used as the reaction fields subdocument schema in the thought model
+
+const reactionSchema = new mongoose.Schema({
+    reactionId:  mongoose.ObjectId,
+    reactionBody: {type: String, required: true, maxLength: 280 },
+    username: { type: String, required: true},
+    createdAt: {type: Date, default: Date.now}
+})
+
+const thoughtSchema = new mongoose.Schema({
+
+    //todo thoughtText
+    //? string
+    //? required
+    //? must be between 1-280 characters
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    //todo createdAt
+    //? date
+    //? set default value to the current time stamp
+    //? use a getter method to format the timestamp on query
+    createdAt: { type: Date, default: Date.now},
+    //todo Username
+    //? string
+    //? required
+    username: { type: String, required: true},
+    //todo reactions
     //? array of nested documents created with the reactionSchema
+    reactions: [reactionSchema]
+});
 
     //! schema settings
         //create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,18 +1,30 @@
-//todo username
-    //? string
-    //? unique
-    //? required
-    //? trimmed
-//todo email
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    //todo username
+        //? string
+        //? unique
+        //? required
+        //? trimmed
+    username: {type: String, required: true, unique: true, trim: true },
+    //todo email
     //? string
     //? required
     //? unique
     //? must match a valid email address
-        //! Mongoose's matching validation
-//todo thoughts
+    email: { type:String, required: true, unique: true, match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/},
+    //! Mongoose's matching validation
+    //todo thoughts
     //? array of id values referencing the thought model
-//todo friends
+    thoughts: [thoughtSchema],
+    //todo friends
     //? array of id values referencing the user model
-
+    friends: [userSchema]
+    
+})
 //! schema settings
     // create a virtual called friendcount that retrieves the length of the users friends array field on query
+
+    // mongoose.Schema.Types.ObjectId
+
+    module.exports = User;
